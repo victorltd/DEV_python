82c1c8906e20c1297c83869d0ac602fc9533ad22
Andre-Goncalves-Martins <88164286+Andre-Goncalves-Martins@users.noreply.github.com>
2021-09-10 16:13:13 -0300

    Add files via upload

---
+++ Comentarios/comentarios.py
@@ -0,0 +1,15 @@
+# Como provavelemente você ja observou em outros exemplos de códigos...
+
+''' 
+Os comentários podem ser tanto comentário em uma única linha
+Como também numa sequencia de linhas
+'''
+
+"""
+Basta você digitar # antes do comentário de linha, ou
+digitar entre 3 pares de aspas simples ou 3 pares de aspas duplas
+para comentar em vá
+"""
+
+# Um comentário de linha após um trecho de código, como por exemplo:
+print('Testando comentário') # Comentando junto de um trecho de codigo
