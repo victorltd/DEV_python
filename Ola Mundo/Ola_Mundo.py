3b4c988e6362c2a2a6cc8a705bbd8f1168766b77
Andre-Goncalves-Martins <88164286+Andre-Goncalves-Martins@users.noreply.github.com>
2021-09-10 16:12:11 -0300

    Add files via upload

---
+++ Ola Mundo/Ola_Mundo.py
@@ -0,0 +1,5 @@
+# Bem-vindo a linguagem de programação Python
+# A seguir podemos ver um simples código usando a função 'print()'
+# Essa função é usada para imprimir na tela o que nós desejarmos para quem estiver executando o código
+
+print('Olá, Mundo!')
