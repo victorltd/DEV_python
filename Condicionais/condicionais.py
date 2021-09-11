3a420b9cd59817d742ba1afd0c1d36d7832c2193
Andre-Goncalves-Martins <88164286+Andre-Goncalves-Martins@users.noreply.github.com>
2021-09-10 21:48:34 -0300

    Add files via upload

---
+++ Condicionais/condicionais.py
@@ -0,0 +1,24 @@
+# Pegando o exemplo apresentado no nosso github, temos:
+
+# Exemplo de if
+idade = 18
+if idade >= 18:
+    print('maior de idade')
+
+# Exemplo de if e else
+idade = 18
+if idade >= 18:
+   print('pode beber')
+else:
+   print('nao pode beber')
+
+# Exemplo de if, elif e else
+idade = 18
+if idade < 12:
+   print('criança')
+elif idade < 18:
+   print('adolescente')
+elif idade < 60:
+   print('adulto')
+else:
+   print('idoso')
