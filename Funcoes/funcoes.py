c68dcf0a86136b965c89f2bb735741e0b60840d5
Andre-Goncalves-Martins <88164286+Andre-Goncalves-Martins@users.noreply.github.com>
2021-09-14 10:17:33 -0300

    Update funcoes.py

--- Funcoes/funcoes.py
+++ Funcoes/funcoes.py
@@ -4,7 +4,7 @@ def hello(meu_nome):
   print('Olá, ' + meu_nome +'\n')# Obs: quando é colocado o '+'12 para o python ele vai cocatenar as strings
 
 def hello2(meu_nome,idade):
-   print('Olá, '+ meu_nome +'\nVoce tem',idade , ' anos')# Observe que antes de idade é uma vírgula, quando o valor no print não for uma string
+   return 'Olá, '+ meu_nome +'\nVoce tem',idade , ' anos'# Observe que antes de idade é uma vírgula, quando o valor no print não for uma string
                                                          # deve-se usar a vírgula para concatená-lo a string do print
 
 # ATENÇÃO: Nensse código será necessária sua interação. Digite o que for requirido quando você executar o código
